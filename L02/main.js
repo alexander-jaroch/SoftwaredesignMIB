baa50bc6bc5e33cf4b3c349bfde108d49804dbf2
Alexander Jaroch <alexander-jaroch@live.com>
2020-04-22 21:52:33 +0200

    added tslint.json and tsconfig.json

--- L02/main.js
+++ L02/main.js
@@ -1,40 +1,40 @@
-var Message = /** @class */ (function () {
-    function Message(lang, message) {
+"use strict";
+class Message {
+    constructor(lang, message) {
         this.lang = lang;
         this.message = message;
     }
-    Message.prototype.greet = function () {
+    greet() {
         return this.message;
-    };
-    Message.prototype.language = function () {
+    }
+    language() {
         return this.lang;
-    };
-    return Message;
-}());
-var MessageCollection = /** @class */ (function () {
-    function MessageCollection() {
+    }
+}
+class MessageCollection {
+    constructor() {
         this.messages = new Array();
     }
-    MessageCollection.prototype.add = function (lang, message) {
+    add(lang, message) {
         this.messages.push(new Message(lang, message));
-    };
-    MessageCollection.prototype.random = function () {
+    }
+    random() {
         return this.messages[this.randomIndex()];
-    };
-    MessageCollection.prototype.randomIndex = function () {
+    }
+    randomIndex() {
         return Math.floor(Math.random() * this.messages.length);
-    };
-    return MessageCollection;
-}());
-var messages = new MessageCollection();
-messages.add('English', 'Hello World!');
-messages.add('German', 'Hallo Welt!');
-messages.add('Greek', 'Γειά σου Κόσμε!');
-messages.add('Italian', 'Ciao mondo!');
-messages.add('Japanese', 'こんにちは世界！');
-messages.add('Russian', 'Привет мир!');
-messages.add('Spanish', '¡Hola Mundo!');
-messages.add('Swedish', 'Hej världen!');
-var rnd = messages.random();
-console.log(rnd.language() === 'English' ? rnd.greet() : '"' + rnd.greet() + '" means "Hello World!" in ' + rnd.language() + '.');
+    }
+}
+const messages = new MessageCollection();
+messages.add("English", "Hello World!");
+messages.add("German", "Hallo Welt!");
+messages.add("Greek", "Γειά σου Κόσμε!");
+messages.add("Italian", "Ciao mondo!");
+messages.add("Japanese", "こんにちは世界！");
+messages.add("Russian", "Привет мир!");
+messages.add("Spanish", "¡Hola Mundo!");
+messages.add("Swedish", "Hej världen!");
+const rnd = messages.random();
+const str = rnd.language() === "English" ? rnd.greet() : "\"" + rnd.greet() + "\" means \"Hello World!\" in " + rnd.language() + ".";
+console.log(str);
 //# sourceMappingURL=main.js.map
--- L02/main.ts
+++ L02/main.ts
@@ -1,17 +1,17 @@
 class Message {
     private lang: string;
-    private message: string
+    private message: string;
 
-    public constructor(lang, message) {
+    public constructor(lang: string, message: string) {
         this.lang = lang;
         this.message = message;
     }
 
-    public greet() {
+    public greet(): string {
         return this.message;
     }
 
-    public language() {
+    public language(): string {
         return this.lang;
     }
 }
@@ -23,29 +23,30 @@ class MessageCollection {
         this.messages = new Array<Message>();
     }
 
-    public add(lang: string, message: string) {
+    public add(lang: string, message: string): void {
         this.messages.push(new Message(lang, message));
     }
 
-    public random() {
+    public random(): Message {
         return this.messages[this.randomIndex()];
     }
 
-    private randomIndex() {
+    private randomIndex(): number {
         return Math.floor(Math.random() * this.messages.length);
     }
 }
 
 const messages: MessageCollection = new MessageCollection();
 
-messages.add('English', 'Hello World!');
-messages.add('German', 'Hallo Welt!');
-messages.add('Greek', 'Γειά σου Κόσμε!');
-messages.add('Italian', 'Ciao mondo!');
-messages.add('Japanese', 'こんにちは世界！');
-messages.add('Russian', 'Привет мир!');
-messages.add('Spanish', '¡Hola Mundo!');
-messages.add('Swedish', 'Hej världen!');
+messages.add("English", "Hello World!");
+messages.add("German", "Hallo Welt!");
+messages.add("Greek", "Γειά σου Κόσμε!");
+messages.add("Italian", "Ciao mondo!");
+messages.add("Japanese", "こんにちは世界！");
+messages.add("Russian", "Привет мир!");
+messages.add("Spanish", "¡Hola Mundo!");
+messages.add("Swedish", "Hej världen!");
 
 const rnd: Message = messages.random();
-console.log(rnd.language() === 'English' ? rnd.greet() : '"' + rnd.greet() + '" means "Hello World!" in ' + rnd.language() + '.');
+const str: string = rnd.language() === "English" ? rnd.greet() : "\"" + rnd.greet() + "\" means \"Hello World!\" in " + rnd.language() + ".";
+console.log(str);
