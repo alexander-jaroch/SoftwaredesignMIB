f8d45e9d45b3b29279fceb8ecb9af093d761cde5
Alexander Jaroch <alexander-jaroch@live.com>
2020-04-29 12:23:42 +0200

    UML to typescript

---
+++ UML/L1/main.js
@@ -0,0 +1,26 @@
+"use strict";
+var UML1;
+(function (UML1) {
+    let eingabe;
+    eingabe = prompt("Eingabe");
+    // erzeuge Array mit Länge von Eingabe
+    const tmpEingabe3 = new Array(eingabe.length);
+    for (let i = 0; i < eingabe.length; i++) {
+        tmpEingabe3[i] = eingabe[i];
+    }
+    tmpEingabe3.reverse();
+    const tmpAusgabe3 = tmpEingabe3.join("");
+    const tmpEingabe2 = eingabe.split(" ");
+    tmpEingabe2.reverse();
+    const tmpAusgabe2 = tmpEingabe2.join(" ");
+    const tmpEingabe1 = new Array(eingabe.length);
+    for (let i = 0; i < eingabe.length; i++) {
+        tmpEingabe1[i] = tmpAusgabe2[i];
+    }
+    tmpEingabe1.reverse();
+    const tmpAusgabe1 = tmpEingabe1.join("");
+    console.log(tmpAusgabe1);
+    console.log(tmpAusgabe2);
+    console.log(tmpAusgabe3);
+})(UML1 || (UML1 = {}));
+//# sourceMappingURL=main.js.map
---
+++ UML/L1/main.ts
@@ -0,0 +1,27 @@
+namespace UML1 {
+    let eingabe: string;
+    eingabe = prompt("Eingabe");
+
+    // erzeuge Array mit Länge von Eingabe
+    const tmpEingabe3: Array<string> = new Array(eingabe.length);
+    for (let i: number = 0; i < eingabe.length; i++) {
+        tmpEingabe3[i] = eingabe[i];
+    }
+    tmpEingabe3.reverse();
+    const tmpAusgabe3: string = tmpEingabe3.join("");
+
+    const tmpEingabe2: Array<string> = eingabe.split(" ");
+    tmpEingabe2.reverse();
+    const tmpAusgabe2: string = tmpEingabe2.join(" ");
+
+    const tmpEingabe1: Array<string> = new Array(eingabe.length);
+    for (let i: number = 0; i < eingabe.length; i++) {
+        tmpEingabe1[i] = tmpAusgabe2[i];
+    }
+    tmpEingabe1.reverse();
+    const tmpAusgabe1: string = tmpEingabe1.join("");
+
+    console.log(tmpAusgabe1);
+    console.log(tmpAusgabe2);
+    console.log(tmpAusgabe3);
+}
